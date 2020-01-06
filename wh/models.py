2d3ede140419079ede2971280cffbf2a84a99bcf
Pavel Oborin <oborin.p@gmail.com>
2020-01-06 19:41:17 +0300

    Changed models && WIP permissions

--- wh/models.py
+++ wh/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import decimal
 
 
 class ProductType(models.Model):
     """Модель для типов товаров."""
-    name = models.CharField(verbose_name='Название', max_length=200, unique=True)
+    name = models.CharField(verbose_name='Название',
+                            max_length=200, unique=True)
 
     class Meta:
         indexes = [models.Index(fields=['name'])]
@@ -16,16 +18,19 @@ class ProductType(models.Model):
 
 class Product(models.Model):
     """Модель конкретных для товаров."""
-    name = models.CharField(verbose_name='Название', max_length=200, unique=True)
-    cost = models.DecimalField(verbose_name='Стоимость', max_digits=9, decimal_places=2)
+    name = models.CharField(verbose_name='Название',
+                            max_length=200, unique=True)
+    cost = models.DecimalField(
+        verbose_name='Стоимость', max_digits=9, decimal_places=2)
+    amount = models.PositiveIntegerField(verbose_name='Кол-во', default=0)
     units = models.CharField(verbose_name='Ед. Измерения', max_length=2,
-        choices=(
-            ('M', 'метр'),
-            ('CM', 'сантиметр'),
-            ('SM', 'квадратный метр'),
-            ('QM', 'кубический метр')
-        )
-    )
+                             choices=(
+                                 ('M', 'метр'),
+                                 ('CM', 'сантиметр'),
+                                 ('SM', 'квадратный метр'),
+                                 ('QM', 'кубический метр')
+                             )
+                             )
     type = models.ForeignKey(
         to='ProductType',
         on_delete=models.PROTECT,
@@ -46,22 +51,27 @@ class Customer(models.Model):
     """Модель для покупаетелей."""
     first_name = models.CharField(verbose_name='Имя', max_length=128)
     last_name = models.CharField(verbose_name='Фамилия', max_length=128)
-    status = models.CharField(verbose_name='Статус', max_length=1,
-        choices=(
-            ('B', 'Бронза'),
-            ('S', 'Серебро'),
-            ('G', 'Золото'),
-            ('P', 'Платина')
-        )
-    )
+    total_spendings = models.PositiveIntegerField(
+        verbose_name='Траты за всё время', default=0)
 
     class Meta:
-        indexes = [models.Index(fields=['first_name', 'last_name'])]
+        indexes = [
+            models.Index(fields=['first_name', 'last_name']),
+            models.Index(fields=['last_name'])
+        ]
         verbose_name = 'Покупатель'
         verbose_name_plural = 'Покупатели'
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} [{self.status}]"
+        return f"{self.first_name} {self.last_name}"
+
+    def calculate_discount(self):
+        """Расчитывает скидку для покупателя"""
+        if self.total_spendings < 5000:
+            return 1
+        if self.total_spendings < 10000:
+            return 0.95
+        return 0.90
 
 
 class Sales(models.Model):
@@ -79,13 +89,17 @@ class Sales(models.Model):
         related_query_name='purchase'
     )
     amount = models.PositiveIntegerField(verbose_name='кол-во')
-    delivery_cost = models.DecimalField(verbose_name='Стоимость доставки', max_digits=9, decimal_places=2)
+    delivery_cost = models.DecimalField(
+        verbose_name='Стоимость доставки', max_digits=9, decimal_places=2, default=0)
     date = models.DateTimeField('Дата', auto_now_add=True)
-    total_cost = models.DecimalField('Стоимость', max_digits=9, decimal_places=2)
-    certificate = models.ForeignKey(
+    total_cost = models.DecimalField(
+        'Стоимость', max_digits=9, decimal_places=2, default=0)
+    certificate = models.OneToOneField(
         to='Sales',
         on_delete=models.PROTECT,
-        blank=True
+        blank=True,
+        null=True,
+        limit_choices_to={'product__type__name': 'Сертификат'}
     )
 
     class Meta:
@@ -94,3 +108,16 @@ class Sales(models.Model):
 
     def __str__(self):
         return f"{self.product} x{self.amount}"
+
+    def save(self, *args, **kwargs):
+        self.total_cost = (self.product.cost * self.amount) * \
+            decimal.Decimal(self.buyer.calculate_discount()) + \
+            self.delivery_cost
+        if self.certificate:
+            self.total_cost = max(0, self.total_cost -
+                                  self.certificate.total_cost)
+        self.buyer.total_spendings += self.total_cost
+        self.buyer.save()
+        self.product.amount -= self.amount
+        self.product.save()
+        super().save(*args, **kwargs)
--- wh/views.py
+++ wh/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import *
 from .models import *
@@ -9,18 +9,22 @@ from .models import *
 class ProductTypeViewSet(viewsets.ModelViewSet):
     queryset = ProductType.objects.all().order_by('name')
     serializer_class = ProductTypeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('name')
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all().order_by('last_name')
     serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class SalesViewSet(viewsets.ModelViewSet):
     queryset = Sales.objects.all().order_by('date')
     serializer_class = SalesSerializer
+    permission_classes = [permissions.IsAuthenticated]
